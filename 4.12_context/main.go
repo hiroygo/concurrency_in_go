f90aebfff35348c01fb4d8eb0d21040698a5118d
hiroygo <pcxn1a@gmail.com>
2020-12-06 13:11:00 +0900

    update

--- 4.12_context/main.go
+++ 4.12_context/main.go
@@ -7,22 +7,12 @@ import (
 	"time"
 )
 
-func workSlowly(ctx context.Context) error {
+func work(ctx context.Context, d time.Duration) error {
 	select {
 	case <-ctx.Done():
 		return ctx.Err()
-	case <-time.After(20 * time.Second):
-		fmt.Println("workSlowly done")
-		return nil
-	}
-}
-
-func workQuickly(ctx context.Context) error {
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	case <-time.After(10 * time.Second):
-		fmt.Println("workQuickly done")
+	case <-time.After(d):
+		fmt.Printf("work %v done\n")
 		return nil
 	}
 }
@@ -33,20 +23,23 @@ func main() {
 	defer cancel()
 
 	wg.Add(1)
+	// キャンセルが実行されてこのゴルーチンもキャンセルされる
 	go func() {
 		defer wg.Done()
-		if err := workSlowly(ctx); err != nil {
-			fmt.Printf("workSlowly error %v\n", err)
+		if err := work(ctx, 10*time.Second); err != nil {
+			fmt.Printf("goroutine1: %v\n", err)
 			cancel()
 		}
 	}()
 
 	wg.Add(1)
+	// このゴルーチンが先にタイムアウトして
+	// キャンセルを実行する
 	go func() {
 		defer wg.Done()
 		ctx, _ := context.WithTimeout(ctx, 5*time.Second)
-		if err := workQuickly(ctx); err != nil {
-			fmt.Printf("workQuickly error %v\n", err)
+		if err := work(ctx, 15*time.Second); err != nil {
+			fmt.Printf("goroutine2: %v\n", err)
 			cancel()
 		}
 	}()
